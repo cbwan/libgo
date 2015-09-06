419f5212da42254c5562333acc0c0dd969c1377c
Cb <git@nowan.net>
2015-09-06 16:27:23 +0200

    - test disconnect

--- libgo/ConsoleGo/ConsoleGo.cpp
+++ libgo/ConsoleGo/ConsoleGo.cpp
@@ -29,14 +29,19 @@ int _tmain(int argc, _TCHAR* argv[])
 	// Starting FSM
 	while(1)
 	{
-		cb_igs_wait_event();
+		//cb_igs_wait_event();
+		Sleep(100);
 
 		int event = cb_igs_get_event();
+		if( event != IGS_EVENT_NO_EVENT )
+		{
+			cout << cb_igs_get_events_nb()  << " >>> 1-Event:" << event << endl;
+		}
 		do
 		{
 			switch( event )
 			{
-			case IGS_EVENT_CONNECTED: cb_igs_login("cbtwo","pandanet"); break;
+			case IGS_EVENT_LOGIN_PROMPT: cb_igs_login("cbtwo","pandanet"); break;
 			case IGS_EVENT_LOGGED_IN: cb_igs_challenge( "cbone", "B" ); break;
 			case IGS_EVENT_CHALLENGE_DECLINED: cout << "SNIIIFFFF match declined" << endl; break;
 			case IGS_EVENT_CHALLENGE_ACCEPTED:
@@ -48,18 +53,27 @@ int _tmain(int argc, _TCHAR* argv[])
 				}
 			case IGS_EVENT_MOVE:
 				{
-					if( string(cb_igs_get_last_move_stone()) != "B" )
+
+					if( cb_igs_get_last_move_stone() != BLACK )
 					{
 						int x = cb_igs_get_last_move_x();
 						int y = cb_igs_get_last_move_y();
 
 						cout << "Opponent played: " << x << ":" << y << endl;
 						play_random();
+						cb_disconnect_igs();
+						exit(0);
 					}
+					break;
 				}
+				default: break;
 			}
 
 			event = cb_igs_get_event();
+			if( event != IGS_EVENT_NO_EVENT )
+			{
+				cout << cb_igs_get_events_nb() << " >>> 2-Event:" << event << endl;
+			}
 		} while( event != IGS_EVENT_NO_EVENT );
 	}
 
